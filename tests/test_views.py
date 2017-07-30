369b0deab39bf32605257fc9b602495559ba60f8
Brigette Eckert <Brigette.Eckert@live.com>
2017-07-30 11:39:26 -0700

    working on setting up test_views

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,3 +1,28 @@
 import unittest
+from django.test import TestCase
+from django.test import RequestFactory
 from freegeek.views import home
 from freegeek.views import diary
+
+
+class HomeViewTest(TestCase):
+    '''
+    Test Home View
+    '''
+    def setup(self):
+        self.user = UserFactory()
+        self.factory = RequestFactory()
+
+    def test_get_home(self):
+        '''Test GET request for home view'''
+
+class DiaryViewTest(TestCase):
+    '''
+    Test Diary View
+    '''
+    def setup(self):
+        self.user = UserFactory()
+        self.factory = RequestFactory()
+
+    def test_get_diary(self):
+        '''Test GET request for diary view'''
