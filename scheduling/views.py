4479e8d4b8ec7130a46e2e84008849f285f01fce
Brigette Eckert <brigette.eckert@live.com>
2018-01-14 19:18:26 -0800

    added a static folder and a few files from old build to freegeek app

--- scheduling/views.py
+++ scheduling/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
 # Create your views here.
+# TODO: Look into SSH Tunneling to get into and play around with existiing scheduling app to base views off of that. -BBE
